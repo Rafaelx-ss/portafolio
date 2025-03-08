414f3a61dde9b4650533b843825bac45d496793e
RafaelASUS <solisrafael895@gmail.com>
2025-03-07 21:29:29 -0600

    Actualizar estilos y colores de los componentes de destacados en la página de inicio

--- src/app/pages/landing/components/highlightswidget.ts
+++ src/app/pages/landing/components/highlightswidget.ts
@@ -11,9 +11,9 @@ import { Component } from '@angular/core';
 
             <div class="grid grid-cols-12 gap-4 my-20 pt-2 md:pt-20">
                 <div class="col-span-12 lg:col-span-6 my-auto flex flex-col text-center lg:text-left lg:items-start gap-4">
-                    <div class="flex items-center justify-center bg-yellow-200 self-center lg:self-start" style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
+                    <div class="flex items-center justify-center bg-[#9c3506] self-center lg:self-start" style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
                         <!-- <i class="pi pi-fw pi-desktop !text-3xl text-yellow-700"></i> -->
-                        <img src="assets/png/brentec.png" class="w-7/12" alt="Brentec logo" />
+                        <img src="assets/png/brentec.png" class="w-7/12" alt="Brentec logo" style="filter: drop-shadow(0 0 10px rgba(0, 0, 0, 1));" />
                     </div>
                     <div class="leading-none text-surface-900 dark:text-surface-0 text-3xl font-normal">Brentec Soluciones Tecnológicas</div>
                     <span class="text-surface-700 dark:text-surface-100 text-2xl leading-normal mr-0 md:mr-2" style="max-width: 650px">
@@ -22,18 +22,18 @@ import { Component } from '@angular/core';
                     </span>
                 </div>
 
-                <a href="https://brentec.mx/" target="_blank" class="flex justify-end order-1 sm:order-2 col-span-12 lg:col-span-6 bg-yellow-100 p-0 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:bg-yellow-200" style="border-radius: 8px">
+                <a href="https://brentec.mx/" target="_blank" class="flex justify-end order-1 sm:order-2 col-span-12 lg:col-span-6 bg-[#9c3506] p-0 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:bg-[#F24F05]" style="border-radius: 8px">
                     <img src="assets/png/mockup-desktop-brentec.png" class="w-11/12 transition-transform duration-300" alt="Brentec mockup" />
                 </a>
             </div>
 
             <div class="grid grid-cols-12 gap-4 mt-20 pb-2 md:pb-20">
-                <a href="https://www.codyexpert.com/site/index.html" target="_blank" class="flex justify-center col-span-12 lg:col-span-6 bg-purple-100 p-0 order-1 lg:order-none transition-transform duration-300 ease-in-out transform hover:scale-105 hover:bg-purple-200" style="border-radius: 8px">
+                <a href="https://www.codyexpert.com/site/index.html" target="_blank" class="flex justify-center col-span-12 lg:col-span-6 bg-[#232745] p-0 order-1 lg:order-none transition-transform duration-300 ease-in-out transform hover:scale-105 hover:bg-[#2E9CFC]" style="border-radius: 8px">
                     <img src="assets/png/mockup-cody.png" class="w-11/12 transition-transform duration-300" alt="CodyExpert mockup" />
                 </a>  
 
                 <div class="col-span-12 lg:col-span-6 my-auto flex flex-col lg:items-end text-center lg:text-right gap-4">
-                    <div class="flex items-center justify-center bg-purple-200 self-center lg:self-end" style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
+                    <div class="flex items-center justify-center bg-[#232745] self-center lg:self-end" style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
                         <!-- <i class="pi pi-fw pi-mobile !text-4xl text-purple-700"></i> -->
                         <img src="assets/png/Cody.png" class="w-7/12" alt="CodyExpert logo" />
                     </div>
